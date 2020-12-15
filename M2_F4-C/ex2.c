b72b3678fdd6c8594af9448f7493846adb3387cd
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2020-12-15 12:43:18 +0000

    Update ex2.c

--- M2_F4-C/ex2.c
+++ M2_F4-C/ex2.c
@@ -6,23 +6,23 @@ int main()
   
     printf("\na nota ");
     scanf("%f", &nota);
-  if(x < 5)
+  if(nota < 5)
   {
       printf("mau");
     }
-  else if(x <= 5 && x < 9.5)
+  else if(nota <= 5 && nota < 9.5)
   {
       printf("medíocre");
     }
-  else if(x <= 9.5 && x < 13.5)
+  else if(nota <= 9.5 && nota < 13.5)
   {
       printf("suficiente");
     }
-  else if(x <= 13.5 && x < 17.5)
+  else if(nota <= 13.5 && nota < 17.5)
   {
       printf("bom");
     }
-  else if(x >= 17.5)
+  else if(nota >= 17.5)
   {
       printf("muito bom");
     }

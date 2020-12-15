44a90b86c04db4309a71975de3d547afff2b10e0
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2020-12-15 12:16:28 +0000

    Update ex2.c

--- M2_F4/ex2.c
+++ M2_F4/ex2.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
-int main() {
-  float x = 1;
-
-  printf("Insira uma nota negativa para sair.\n\n");
-
-  while(x) {
-    printf("\nInsira a nota do aluno: ");
-    scanf("%f", &x);
-    if(x < 0) {
-      return 0;
-    } else if(x < 5) {
-      printf("Mau!");
-    } else if(x <= 5 && x < 9.5) {
-      printf("Medíocre!");
-    } else if(x <= 9.5 && x < 13.5) {
-      printf("Suficiente!");
-    } else if(x <= 13.5 && x < 17.5) {
-      printf("Bom!");
-    } else if(x >= 17.5) {
-      printf("Muito bom!");
+int main()
+{
+  float nota;
+  
+    printf("\na nota ");
+    scanf("%f", &nota);
+  if(x < 5)
+  {
+      printf("mau");
     }
-  }
-
-  return 0;
+  else if(x <= 5 && x < 9.5)
+  {
+      printf("medíocre");
+    }
+  else if(x <= 9.5 && x < 13.5)
+  {
+      printf("suficiente");
+    }
+  else if(x <= 13.5 && x < 17.5)
+  {
+      printf("bom");
+    }
+  else if(x >= 17.5)
+  {
+      printf("muito bom");
+    }
 }

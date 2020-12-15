e4ac259d32acd75820cc6db938dd1191aaf595ec
Sup3rPlays <73548496+SPFck@users.noreply.github.com>
2020-12-15 12:14:53 +0000

    Update ex3.c

--- M2_F4/ex3.c
+++ M2_F4/ex3.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 
 int main() {
-  int x = 1;
-
-  printf("Insira 0 para sair.\n");
-
-  while(x) {
-    printf("\nInsira o número do mês: ");
-    scanf("%d", &x);
-    if(x != 0) {
+  int mes;
+    printf("\nnumero do mes ");
+    scanf("%d", &mes);
       switch(x) {
-        case 1: printf("Janeiro."); break;
-        case 2: printf("Fevereiro."); break;
-        case 3: printf("Março."); break;
-        case 4: printf("Abril."); break;
-        case 5: printf("Maio."); break;
-        case 6: printf("Junho."); break;
-        case 7: printf("Julho."); break;
-        case 8: printf("Agosto."); break;
-        case 9: printf("Setembro."); break;
-        case 10: printf("Outubro."); break;
-        case 11: printf("Novembro."); break;
-        case 12: printf("Dezembro"); break;
+        case 1: printf("janeiro");
+          break;
+        case 2: printf("fevereiro");
+          break;
+        case 3: printf("março");
+          break;
+        case 4: printf("abril");
+          break;
+        case 5: printf("maio");
+          break;
+        case 6: printf("junho");
+          break;
+        case 7: printf("julho");
+          break;
+        case 8: printf("agosto");
+          break;
+        case 9: printf("setembro");
+          break;
+        case 10: printf("outubro");
+          break;
+        case 11: printf("novembro");
+          break;
+        case 12: printf("dezembro");
+          break;
 
-        default: printf("Mês inexistente!");
+        default: printf("mes errado");
       }
-    }
-  }
 
   return 0;
 }
